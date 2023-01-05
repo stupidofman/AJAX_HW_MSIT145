e864f8cabb78a2b7f9d0a7f3962a78bc710e5c3b
stupidofman <117073334+stupidofman@users.noreply.github.com>
2023-01-05 14:57:58 +0800

    加入專案檔案。

---
+++ AJAX_HW_MSIT145_何彥儀/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using AJAX_HW_MSIT145_何彥儀.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AJAX_HW_MSIT145_何彥儀.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AJAX_HW_MSIT145_何彥儀/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AJAX_HW_MSIT145_何彥儀.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
